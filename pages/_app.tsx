22d4687c5809e2662906437afd1b2f7aa9e822ad
ryo yoneyama <uryonet@gmail.com>
2021-02-26 22:24:55 +0900

    #4 appにstoreを紐付け

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import {AppProps} from "next/app"
+import {Provider} from "react-redux"
+import store from "../store/store"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({Component, pageProps}: AppProps): JSX.Element => {
+    return (
+        <Provider store={store}>
+            <Component {...pageProps} />
+        </Provider>
+    )
 }
 
 export default MyApp
