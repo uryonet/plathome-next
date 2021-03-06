cd692fb08c007e74d014b6feea20510b517c699b
ryo yoneyama <uryonet@gmail.com>
2021-03-06 13:26:33 +0900

    #6 カスタムAppとしてLayoutコンポーネントを適用

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import '../styles/globals.css'
 import { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '../components/Layout'
 import store from '../store/store'
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <Provider store={store}>
       <ChakraProvider resetCSS>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ChakraProvider>
     </Provider>
   )
