8ae2c6056017cef40a7faf045faa7a95b0ab572b
ryo yoneyama <uryonet@gmail.com>
2021-03-06 17:05:31 +0900

    #6 フォーマッターを適用

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,60 +8,53 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <Heading as='h1' className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
+        <Heading as="h1" className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </Heading>
 
-        <Button colorScheme='blue'>Button</Button>
+        <Button colorScheme="blue">Button</Button>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by editing <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
-          <a href='https://nextjs.org/docs' className={styles.card}>
-            <Heading as='h3'>Documentation &rarr;</Heading>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <Heading as="h3">Documentation &rarr;</Heading>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href='https://nextjs.org/learn' className={styles.card}>
-            <Heading as='h3'>Learn &rarr;</Heading>
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <Heading as="h3">Learn &rarr;</Heading>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href='https://github.com/vercel/next.js/tree/master/examples'
-            className={styles.card}
-          >
-            <Heading as='h3'>Examples &rarr;</Heading>
+          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
+            <Heading as="h3">Examples &rarr;</Heading>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
-            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <Heading as='h3'>Deploy &rarr;</Heading>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <Heading as="h3">Deploy &rarr;</Heading>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
+          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
