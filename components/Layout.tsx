dd726d6003f6879985228c6f12ee1725b5bf8676
ryo yoneyama <uryonet@gmail.com>
2021-03-08 22:02:40 +0900

    #13 msalを使用してログイン処理をの実装

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,7 +3,14 @@ import Head from 'next/head'
 import Header from './Header'
 import { Box } from '@chakra-ui/layout'
 
+import { MsalAuthenticationTemplate } from '@azure/msal-react'
+import { InteractionType } from '@azure/msal-browser'
+
 const Layout: React.FC = ({ children }) => {
+  const authRequest = {
+    scopes: ['openid', 'profile']
+  }
+
   return (
     <Box h="100vh">
       <Head>
@@ -12,7 +19,12 @@ const Layout: React.FC = ({ children }) => {
         <meta httpEquiv="content-language" content="ja" />
       </Head>
       <Header />
-      <div>{children}</div>
+      <MsalAuthenticationTemplate
+        interactionType={InteractionType.Redirect}
+        authenticationRequest={authRequest}
+      >
+        <div>{children}</div>
+      </MsalAuthenticationTemplate>
     </Box>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Heading } from '@chakra-ui/layout'
 import styles from '../styles/Home.module.css'
+import { useIsAuthenticated } from '@azure/msal-react'
 
 const Home: React.FC = () => {
+  const isAuthenticated = useIsAuthenticated()
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <Heading as="h1">Welcome to plathome!</Heading>
+        {isAuthenticated && (
+          <p>ログイン済みです。</p>
+        )}
+        {!isAuthenticated && (
+          <p>ログインが必要です。</p>
+        )}
       </main>
     </div>
   )
