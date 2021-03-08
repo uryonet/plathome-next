facd563c127427e127cc556caf575e07eb6b6ace
ryo yoneyama <uryonet@gmail.com>
2021-03-08 16:25:41 +0900

    #6 トップページのスタイルを調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
-import { Box, Heading } from '@chakra-ui/layout'
+import { Heading } from '@chakra-ui/layout'
 import styles from '../styles/Home.module.css'
 
 const Home: React.FC = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <Heading as="h1" className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </Heading>
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
+        <Heading as="h1">Welcome to plathome!</Heading>
       </main>
     </div>
   )
