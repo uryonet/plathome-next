fc4a6b2693f4ad5826024fbd3bfd036f22ed0436
ryo yoneyama <uryonet@gmail.com>
2021-03-10 22:06:48 +0900

    #17 msalの処理を一時的に削除

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,14 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import Header from './Header'
 
-import { MsalAuthenticationTemplate } from '@azure/msal-react'
-import { InteractionType } from '@azure/msal-browser'
-
 const Layout: React.FC = ({ children }) => {
-  const authRequest = {
-    scopes: ['openid', 'profile']
-  }
-
   return (
     <div className="flex flex-col h-screen">
       <Head>
@@ -18,12 +11,7 @@ const Layout: React.FC = ({ children }) => {
         <meta httpEquiv="content-language" content="ja" />
       </Head>
       <Header />
-      <MsalAuthenticationTemplate
-        interactionType={InteractionType.Redirect}
-        authenticationRequest={authRequest}
-      >
-        <div className="container h-full">{children}</div>
-      </MsalAuthenticationTemplate>
+      <div className="container h-full">{children}</div>
     </div>
   )
 }
