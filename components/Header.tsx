cfd050e0d73e8cac3a411fdf4c3ccbf18a2fff38
uryonet <uryonet@outlook.com>
2021-03-22 22:51:52 +0900

    #22 bulma用のカスタマイズ設定を作成し、テストbuttonを作成

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,7 @@ const Header: React.FC = () => {
   return (
     <nav className="flex fixed w-full items-center justify-between px-2 h-16 border-b border-gray-200">
       <h1>plathome</h1>
-      <button className="ml-auto py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white rounded">
-        menu
-      </button>
+      <button className="button is-primary">menu</button>
     </nav>
   )
 }
